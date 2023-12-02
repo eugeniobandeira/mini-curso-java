cddf6d9f16b2e81dee5d8fe564ae5b644ba9710a
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-12-02 16:57:46 -0300

    Listando as tarefas (Get)

--- todolist/src/main/java/br/com/eugenio/todolist/task/TaskController.java
+++ todolist/src/main/java/br/com/eugenio/todolist/task/TaskController.java
@@ -1,11 +1,14 @@
 package br.com.eugenio.todolist.task;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,5 +43,15 @@ public class TaskController {
         
         var task = this.taskRepository.save(taskModel);
         return ResponseEntity.status(HttpStatus.OK).body(task);
+    }
+
+    @GetMapping("/")
+    public List<TaskModel> list(HttpServletRequest request) {
+
+        System.out.println("Chegou no get task");
+        var idUser = request.getAttribute("idUser");
+        var tasks = this.taskRepository.findByIdUser((UUID) idUser);
+
+        return tasks;
     }
 }
