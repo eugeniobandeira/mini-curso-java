f88e2170fd00ba1d57ef3181adbe03f61c262cdb
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2023-12-02 14:30:25 -0300

    Decode da senha

--- todolist/src/main/java/br/com/eugenio/todolist/filter/FilterTaskAuth.java
+++ todolist/src/main/java/br/com/eugenio/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,12 @@ package br.com.eugenio.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.eugenio.todolist.users.IUserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -14,6 +17,9 @@ import jakarta.servlet.http.HttpServletResponse;
 // Essa anotação diz para o spring gerenciar a classe
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+    @Autowired
+    private IUserRepository userRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -25,18 +31,26 @@ public class FilterTaskAuth extends OncePerRequestFilter {
                 byte[] authDecode = Base64.getDecoder().decode(authEncoded);
                 var authString = new String(authDecode);
 
-                System.out.println(authString);
-
                 String[] credentials = authString.split(":");
                 String username = credentials[0];
                 String password = credentials[1];
 
-                System.out.println(username);
-                System.out.println(password);
                 //Validar usuário
+                var user = this.userRepository.findByUsername(username);
+                if(user == null) {
+                    response.sendError(401);
+                } else {
+                //Validar senha
+                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
+                    if(passwordVerify.verified) {
+                        filterChain.doFilter(request, response);
+                    } else {
+                        response.sendError(401);
+                    }   
+                }
                 //Segue viagem
-                filterChain.doFilter(request, response);
+                
             }
 
     
